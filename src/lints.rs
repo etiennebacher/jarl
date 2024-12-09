8103f2bd86ea9dc0ab6a3950856aec2634f1a3e8
etiennebacher <etienne.bacher@protonmail.com>
2024-12-09 22:30:19 +0100

    minor refactor

--- src/lints.rs
+++ src/lints.rs
@@ -71,14 +71,14 @@ impl LintChecker for AnyDuplicated {
 impl LintChecker for TrueFalseSymbol {
     fn check(&self, ast: &RSyntaxNode, loc_new_lines: &Vec<usize>, file: &str) -> Vec<Message> {
         let mut messages = vec![];
-        if ast.kind() == RSyntaxKind::R_IDENTIFIER {
-            if ast.text_trimmed() == "T" || ast.text_trimmed() == "F" {
-                let (row, column) = find_row_col(ast, loc_new_lines);
-                messages.push(Message::TrueFalseSymbol {
-                    filename: file.into(),
-                    location: Location { row, column },
-                });
-            }
+        if ast.kind() == RSyntaxKind::R_IDENTIFIER
+            && (ast.text_trimmed() == "T" || ast.text_trimmed() == "F")
+        {
+            let (row, column) = find_row_col(ast, loc_new_lines);
+            messages.push(Message::TrueFalseSymbol {
+                filename: file.into(),
+                location: Location { row, column },
+            });
         }
         messages
     }
