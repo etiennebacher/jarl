bab069e255f93f4b803fcb1eb360f53fe684ace6
etiennebacher <etienne.bacher@protonmail.com>
2025-08-23 16:46:54 +0200

    refactor

--- src/lints/is_numeric/is_numeric.rs
+++ src/lints/is_numeric/is_numeric.rs
@@ -50,35 +50,26 @@ pub fn is_numeric(ast: &RBinaryExpression) -> Result<Option<Diagnostic>> {
         return Ok(None);
     };
 
-    let lhs = left.clone().into_syntax();
-    let rhs = right.clone().into_syntax();
-
-    // Early return: LHS or RHS are not functions
-    let left_is_function = lhs.kind() == RSyntaxKind::R_CALL;
-    let right_is_function = rhs.kind() == RSyntaxKind::R_CALL;
-
-    if !left_is_function || !right_is_function {
+    // Early returns: LHS or RHS are not functions
+    let Some(left) = left.as_r_call() else {
+        return Ok(None);
+    };
+    let Some(right) = right.as_r_call() else {
         return Ok(None);
     };
 
-    // Early return: LHS or RHS are not the correct functions
-    let left_is_numeric = lhs
-        .first_child()
-        .map(|x| x.text_trimmed() == "is.numeric")
-        .unwrap_or(false);
-    let right_is_numeric = rhs
-        .first_child()
-        .map(|x| x.text_trimmed() == "is.numeric")
-        .unwrap_or(false);
+    let RCallFields { function: fun_left, arguments: arg_left } = left.as_fields();
+    let fun_left = fun_left?;
+    let arg_left = arg_left?;
+    let RCallFields { function: fun_right, arguments: arg_right } = right.as_fields();
+    let fun_right = fun_right?;
+    let arg_right = arg_right?;
 
-    let left_is_integer = lhs
-        .first_child()
-        .map(|x| x.text_trimmed() == "is.integer")
-        .unwrap_or(false);
-    let right_is_integer = rhs
-        .first_child()
-        .map(|x| x.text_trimmed() == "is.integer")
-        .unwrap_or(false);
+    // Early return: LHS or RHS are not the correct functions
+    let left_is_numeric = fun_left.to_trimmed_text() == "is.numeric";
+    let right_is_numeric = fun_right.to_trimmed_text() == "is.numeric";
+    let left_is_integer = fun_left.to_trimmed_text() == "is.integer";
+    let right_is_integer = fun_right.to_trimmed_text() == "is.integer";
 
     if !((left_is_integer && right_is_numeric) || (left_is_numeric && right_is_integer)) {
         return Ok(None);
@@ -86,20 +77,8 @@ pub fn is_numeric(ast: &RBinaryExpression) -> Result<Option<Diagnostic>> {
 
     // Early return: LHS and RHS args are not the same (e.g.
     // `is.numeric(x) || is.integer(y)`).
-    let left_arg = left
-        .as_r_call()
-        .unwrap()
-        .arguments()?
-        .into_syntax()
-        .text_trimmed();
-
-    let right_arg = right
-        .as_r_call()
-        .unwrap()
-        .arguments()?
-        .into_syntax()
-        .text_trimmed();
-
+    let left_arg = arg_left.into_syntax().text_trimmed();
+    let right_arg = arg_right.into_syntax().text_trimmed();
     if left_arg != right_arg {
         return Ok(None);
     };
