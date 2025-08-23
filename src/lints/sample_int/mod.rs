86079e39c1d1a2019b08f98c42b43199dbc1066a
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2025-08-23 17:54:04 +0200

    Implement `sample_int_linter` (#60)

---
+++ src/lints/sample_int/mod.rs
@@ -0,0 +1,57 @@
+pub(crate) mod sample_int;
+
+#[cfg(test)]
+mod tests {
+    use crate::utils_test::*;
+
+    #[test]
+    fn test_no_lint_sample_int() {
+        expect_no_lint("sample('a', m)", "sample_int", None);
+        expect_no_lint("sample(1, m)", "sample_int", None);
+        expect_no_lint("sample(n, m)", "sample_int", None);
+        expect_no_lint("sample(n, m, TRUE)", "sample_int", None);
+        expect_no_lint("sample(n, m, prob = 1:n/n)", "sample_int", None);
+        expect_no_lint("sample(foo(x), m, TRUE)", "sample_int", None);
+        expect_no_lint("sample(n, replace = TRUE)", "sample_int", None);
+        expect_no_lint("sample(10:1, m)", "sample_int", None);
+        expect_no_lint("sample(replace = TRUE, letters)", "sample_int", None);
+        expect_no_lint("x$sample(1:2, 1)", "sample_int", None);
+    }
+
+    #[test]
+    fn test_lint_sample_int() {
+        use insta::assert_snapshot;
+
+        let expected_message = "is preferable to `sample(1:n, m, ...)`";
+        expect_lint("sample(1:10, 2)", expected_message, "sample_int", None);
+        expect_lint("sample(1L:10L, 2)", expected_message, "sample_int", None);
+        expect_lint("sample(1:n, 2)", expected_message, "sample_int", None);
+        expect_lint(
+            "sample(1:k, replace = TRUE)",
+            expected_message,
+            "sample_int",
+            None,
+        );
+        expect_lint(
+            "sample(1:foo(x), prob = bar(x))",
+            expected_message,
+            "sample_int",
+            None,
+        );
+        assert_snapshot!(
+            "fix_output",
+            get_fixed_text(
+                vec![
+                    "sample(1:10, 2)",
+                    "sample(1L:10L, 2)",
+                    "sample(n = 1:10, 2)",
+                    "sample(2, n = 1:10)",
+                    "sample(size = 2, n = 1:10)",
+                    "sample(replace = TRUE, letters)",
+                ],
+                "sample_int",
+                None
+            )
+        );
+    }
+}
---
+++ src/lints/sample_int/sample_int.rs
@@ -0,0 +1,100 @@
+use crate::message::*;
+use crate::utils::{drop_arg_by_name_or_position, get_arg_by_name_then_position};
+use air_r_syntax::*;
+use anyhow::Result;
+use biome_rowan::AstNode;
+
+pub struct SampleInt;
+
+/// ## What it does
+///
+/// Checks for usage of `sample(1:n, m, ...)` and replaces it with
+/// `sample.int(n, m, ...)` for readability.
+///
+/// ## Why is this bad?
+///
+/// `sample()` calls `sample.int()` internally so they have the same performance,
+/// but the latter is more readable.
+///
+/// ## Example
+///
+/// ```r
+/// sample(1:10, 2)
+/// ```
+///
+/// Use instead:
+/// ```r
+/// sample.int(10, 2)
+/// ```
+///
+/// ## References
+///
+/// See `?sample`
+impl Violation for SampleInt {
+    fn name(&self) -> String {
+        "sample_int".to_string()
+    }
+    fn body(&self) -> String {
+        "`sample.int(n, m, ...)` is preferable to `sample(1:n, m, ...)`.".to_string()
+    }
+}
+
+pub fn sample_int(ast: &RCall) -> Result<Option<Diagnostic>> {
+    let RCallFields { function, arguments } = ast.as_fields();
+
+    let function = function?;
+    let args = arguments?.items();
+
+    if function.to_trimmed_text() != "sample" {
+        return Ok(None);
+    }
+
+    let n = get_arg_by_name_then_position(&args, "n", 1);
+
+    // Is the `n` argument of the form `1:x`? If so, keep the `x` part so it
+    // can be reused in the fix.
+    let right_value = if let Some(n) = n {
+        let n_value = n.value().unwrap();
+        if let Some(n_value) = n_value.as_r_binary_expression() {
+            let RBinaryExpressionFields { left, operator, right } = n_value.as_fields();
+            let left = left?;
+            if left.to_trimmed_text() != "1" && left.to_trimmed_text() != "1L" {
+                return Ok(None);
+            }
+            if operator?.kind() != RSyntaxKind::COLON {
+                return Ok(None);
+            }
+            right?.to_trimmed_text().to_string()
+        } else {
+            return Ok(None);
+        }
+    } else {
+        return Ok(None);
+    };
+
+    let other_args = drop_arg_by_name_or_position(&args, "n", 1);
+    let inner_content = match other_args {
+        Some(x) => {
+            let out = x
+                .iter()
+                .map(|x| x.clone().into_syntax().text_trimmed().to_string())
+                .collect::<Vec<_>>()
+                .join(", ");
+
+            [right_value, out].join(", ")
+        }
+        None => right_value,
+    };
+    let range = ast.clone().into_syntax().text_trimmed_range();
+    let diagnostic = Diagnostic::new(
+        SampleInt,
+        range,
+        Fix {
+            content: format!("sample.int({inner_content})"),
+            start: range.start().into(),
+            end: range.end().into(),
+        },
+    );
+
+    Ok(Some(diagnostic))
+}
