13dbc7e3b07ef4f34a4d5acf95daffb5188dea46
etiennebacher <etienne.bacher@protonmail.com>
2025-09-28 21:33:40 +0200

    remove lsp folder

--- lsp/src/notifications.rs
+++
@@ -1,8 +0,0 @@
-//
-// mod.rs
-//
-// Copyright (C) 2024 Posit Software, PBC. All rights reserved.
-//
-//
-
-pub mod sync_file_settings;
--- lsp/src/test.rs
+++
@@ -1,8 +0,0 @@
-mod client;
-mod client_ext;
-mod utils;
-
-pub(crate) use client::new_test_client;
-pub(crate) use client_ext::FileName;
-pub(crate) use client_ext::TestClientExt;
-pub(crate) use utils::extract_marked_range;
