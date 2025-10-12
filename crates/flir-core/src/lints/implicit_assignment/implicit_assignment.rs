896daa8744f84f8e81ef7370305c0b61ab5b3f2a
etiennebacher <etienne.bacher@protonmail.com>
2025-10-12 20:56:00 +0200

    init

---
+++ crates/flir-core/src/lints/implicit_assignment/implicit_assignment.rs
@@ -0,0 +1,103 @@
+use crate::diagnostic::*;
+use air_r_syntax::*;
+use biome_rowan::AstNode;
+
+/// ## What it does
+///
+/// Checks for implicit assignment in function calls and other situations.
+///
+/// ## Why is this bad?
+///
+/// Assigning inside function calls or other situations such as in `if()` makes
+/// the code difficult to read, and should be avoided.
+///
+/// ## Example
+///
+/// ```r
+/// mean(x <- c(1, 2, 3))
+/// x
+///
+/// if (any(y <- x > 0)) {
+///   print(y)
+/// }
+/// ```
+///
+/// Use instead:
+/// ```r
+/// x <- c(1, 2, 3)
+/// mean(x)
+/// x
+///
+/// larger <- x > 0
+/// if (any(larger)) {
+///   print(larger)
+/// }
+/// ```
+///
+/// ## References
+///
+/// See:
+/// * https://style.tidyverse.org/syntax.html#assignment
+pub fn implicit_assignment(ast: &RBinaryExpression) -> anyhow::Result<Option<Diagnostic>> {
+    let operator = ast.operator()?;
+    if operator.kind() != RSyntaxKind::ASSIGN
+        && operator.kind() != RSyntaxKind::SUPER_ASSIGN
+        && operator.kind() != RSyntaxKind::ASSIGN_RIGHT
+        && operator.kind() != RSyntaxKind::SUPER_ASSIGN_RIGHT
+    {
+        return Ok(None);
+    };
+
+    let ancestor_is_arg = {
+        let mut result = false;
+        for ancestor in ast.syntax().ancestors() {
+            if RBracedExpressions::can_cast(ancestor.kind()) {
+                // Found braced expressions first, so skip
+                result = false;
+                break;
+            } else if RArgument::can_cast(ancestor.kind()) {
+                // Found argument first, so include
+                result = true;
+                break;
+            }
+        }
+        result
+    };
+    let ancestor_is_while = ast
+        .syntax()
+        .ancestors()
+        .any(|x| RWhileStatement::can_cast(x.kind()));
+    let ancestor_is_if = ast
+        .syntax()
+        .ancestors()
+        .any(|x| RIfStatement::can_cast(x.kind()));
+    let ancestor_is_for = ast
+        .syntax()
+        .ancestors()
+        .any(|x| RForStatement::can_cast(x.kind()));
+
+    if !ancestor_is_if && !ancestor_is_while && !ancestor_is_arg && !ancestor_is_for {
+        return Ok(None);
+    }
+
+    let msg = if ancestor_is_if {
+        "Avoid implicit assignments in `if()` statements."
+    } else if ancestor_is_while {
+        "Avoid implicit assignments in `while()` statements."
+    } else if ancestor_is_for {
+        "Avoid implicit assignments in `for()` statements."
+    } else if ancestor_is_arg {
+        "Avoid implicit assignments in function calls."
+    } else {
+        unreachable!()
+    };
+
+    let range = ast.syntax().text_trimmed_range();
+    let diagnostic = Diagnostic::new(
+        ViolationData::new("implicit_assignment".to_string(), msg.to_string()),
+        range,
+        Fix::empty(),
+    );
+
+    Ok(Some(diagnostic))
+}
