51aac43ac29d2b1c789389df1d4f796f2f596dd9
etiennebacher <etienne.bacher@protonmail.com>
2025-10-15 22:34:27 +0200

    init

--- crates/flir-core/src/lints/implicit_assignment/implicit_assignment.rs
+++ crates/flir-core/src/lints/implicit_assignment/implicit_assignment.rs
@@ -39,155 +39,157 @@ use biome_rowan::AstNode;
 /// See:
 /// * https://style.tidyverse.org/syntax.html#assignment
 pub fn implicit_assignment(ast: &RBinaryExpression) -> anyhow::Result<Option<Diagnostic>> {
-    let operator = ast.operator()?;
-    if operator.kind() != RSyntaxKind::ASSIGN
-        && operator.kind() != RSyntaxKind::SUPER_ASSIGN
-        && operator.kind() != RSyntaxKind::ASSIGN_RIGHT
-        && operator.kind() != RSyntaxKind::SUPER_ASSIGN_RIGHT
-    {
-        return Ok(None);
-    };
+    Ok(None)
 
-    // We want to report the use of assignment in function arguments, but not
-    // when they're part of the body of some functions, e.g.
-    // ```
-    // local({
-    //   x <- 1
-    // })
-    // ```
-    // so we set `ancestor_is_arg = false` if an RBracedExpressions is a closer
-    // ancestor.
-    let ancestor_is_arg = {
-        let mut result = false;
-        for ancestor in ast.syntax().ancestors() {
-            if RBracedExpressions::can_cast(ancestor.kind()) {
-                // Found braced expressions first, so skip
-                result = false;
-                break;
-            } else if RArgument::can_cast(ancestor.kind()) {
-                // Found argument first, so include
-                result = true;
-                break;
-            }
-        }
-        result
-    };
+    // let operator = ast.operator()?;
+    // if operator.kind() != RSyntaxKind::ASSIGN
+    //     && operator.kind() != RSyntaxKind::SUPER_ASSIGN
+    //     && operator.kind() != RSyntaxKind::ASSIGN_RIGHT
+    //     && operator.kind() != RSyntaxKind::SUPER_ASSIGN_RIGHT
+    // {
+    //     return Ok(None);
+    // };
 
-    // We want to skip cases like
-    // ```r
-    // if (TRUE) x <- 1
-    // for (i in 1:2) x <- 1
-    // while (TRUE) x <- 1
-    // ```
-    // i.e., we want to skip cases that are in the body of RIfStatement /
-    // RForStatement / RWhileStatement.
+    // // We want to report the use of assignment in function arguments, but not
+    // // when they're part of the body of some functions, e.g.
+    // // ```
+    // // local({
+    // //   x <- 1
+    // // })
+    // // ```
+    // // so we set `ancestor_is_arg = false` if an RBracedExpressions is a closer
+    // // ancestor.
+    // let ancestor_is_arg = {
+    //     let mut result = false;
+    //     for ancestor in ast.syntax().ancestors() {
+    //         if RBracedExpressions::can_cast(ancestor.kind()) {
+    //             // Found braced expressions first, so skip
+    //             result = false;
+    //             break;
+    //         } else if RArgument::can_cast(ancestor.kind()) {
+    //             // Found argument first, so include
+    //             result = true;
+    //             break;
+    //         }
+    //     }
+    //     result
+    // };
 
-    let ancestor_is_if = {
-        let mut result = false;
+    // // We want to skip cases like
+    // // ```r
+    // // if (TRUE) x <- 1
+    // // for (i in 1:2) x <- 1
+    // // while (TRUE) x <- 1
+    // // ```
+    // // i.e., we want to skip cases that are in the body of RIfStatement /
+    // // RForStatement / RWhileStatement.
 
-        // The `consequence` part of an `RIfStatement` is always the 5th node
-        // (index 4):
-        // IF_KW - L_PAREN - [condition] - R_PAREN - [consequence]
-        //
-        // `.unwrap()` is fine here because the RBinaryExpression will always
-        // have a parent.
-        let in_if_body = ast.syntax().parent().unwrap().kind() == RSyntaxKind::R_IF_STATEMENT
-            && ast.syntax().index() == 4;
-        let in_else_body = ast.syntax().parent().unwrap().kind() == RSyntaxKind::R_ELSE_CLAUSE
-            && ast.syntax().index() == 1;
+    // let ancestor_is_if = {
+    //     let mut result = false;
 
-        if !in_if_body && !in_else_body {
-            for ancestor in ast.syntax().ancestors() {
-                if RBracedExpressions::can_cast(ancestor.kind()) {
-                    // Found braced expressions first, so skip
-                    result = false;
-                    break;
-                } else if RIfStatement::can_cast(ancestor.kind()) {
-                    // Found argument first, so include
-                    result = true;
-                    break;
-                }
-            }
-        }
-        result
-    };
-    let ancestor_is_while = {
-        let mut result = false;
+    //     // The `consequence` part of an `RIfStatement` is always the 5th node
+    //     // (index 4):
+    //     // IF_KW - L_PAREN - [condition] - R_PAREN - [consequence]
+    //     //
+    //     // `.unwrap()` is fine here because the RBinaryExpression will always
+    //     // have a parent.
+    //     let in_if_body = ast.syntax().parent().unwrap().kind() == RSyntaxKind::R_IF_STATEMENT
+    //         && ast.syntax().index() == 4;
+    //     let in_else_body = ast.syntax().parent().unwrap().kind() == RSyntaxKind::R_ELSE_CLAUSE
+    //         && ast.syntax().index() == 1;
 
-        // The `consequence` part of an `RWhileStatement` is always the 5th node
-        // (index 4):
-        // WHILE_KW - L_PAREN - [condition] - R_PAREN - [consequence]
-        //
-        // `.unwrap()` is fine here because the RBinaryExpression will always
-        // have a parent.
-        let in_while_body = ast.syntax().parent().unwrap().kind() == RSyntaxKind::R_WHILE_STATEMENT
-            && ast.syntax().index() == 4;
+    //     if !in_if_body && !in_else_body {
+    //         for ancestor in ast.syntax().ancestors() {
+    //             if RBracedExpressions::can_cast(ancestor.kind()) {
+    //                 // Found braced expressions first, so skip
+    //                 result = false;
+    //                 break;
+    //             } else if RIfStatement::can_cast(ancestor.kind()) {
+    //                 // Found argument first, so include
+    //                 result = true;
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     result
+    // };
+    // let ancestor_is_while = {
+    //     let mut result = false;
 
-        if !in_while_body {
-            for ancestor in ast.syntax().ancestors() {
-                if RBracedExpressions::can_cast(ancestor.kind()) {
-                    // Found braced expressions first, so skip
-                    result = false;
-                    break;
-                } else if RWhileStatement::can_cast(ancestor.kind()) {
-                    // Found argument first, so include
-                    result = true;
-                    break;
-                }
-            }
-        }
-        result
-    };
-    let ancestor_is_for = {
-        let mut result = false;
+    //     // The `consequence` part of an `RWhileStatement` is always the 5th node
+    //     // (index 4):
+    //     // WHILE_KW - L_PAREN - [condition] - R_PAREN - [consequence]
+    //     //
+    //     // `.unwrap()` is fine here because the RBinaryExpression will always
+    //     // have a parent.
+    //     let in_while_body = ast.syntax().parent().unwrap().kind() == RSyntaxKind::R_WHILE_STATEMENT
+    //         && ast.syntax().index() == 4;
 
-        // The `consequence` part of an `RWhileStatement` is always the 7th node
-        // (index 6):
-        // FOR_KW - L_PAREN - [value] - IN_KW - [sequence] - R_PAREN - [consequence]
-        //
-        // `.unwrap()` is fine here because the RBinaryExpression will always
-        // have a parent.
-        let in_for_body = ast.syntax().parent().unwrap().kind() == RSyntaxKind::R_FOR_STATEMENT
-            && ast.syntax().index() == 6;
+    //     if !in_while_body {
+    //         for ancestor in ast.syntax().ancestors() {
+    //             if RBracedExpressions::can_cast(ancestor.kind()) {
+    //                 // Found braced expressions first, so skip
+    //                 result = false;
+    //                 break;
+    //             } else if RWhileStatement::can_cast(ancestor.kind()) {
+    //                 // Found argument first, so include
+    //                 result = true;
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     result
+    // };
+    // let ancestor_is_for = {
+    //     let mut result = false;
 
-        if !in_for_body {
-            for ancestor in ast.syntax().ancestors() {
-                if RBracedExpressions::can_cast(ancestor.kind()) {
-                    // Found braced expressions first, so skip
-                    result = false;
-                    break;
-                } else if RForStatement::can_cast(ancestor.kind()) {
-                    // Found argument first, so include
-                    result = true;
-                    break;
-                }
-            }
-        }
-        result
-    };
+    //     // The `consequence` part of an `RWhileStatement` is always the 7th node
+    //     // (index 6):
+    //     // FOR_KW - L_PAREN - [value] - IN_KW - [sequence] - R_PAREN - [consequence]
+    //     //
+    //     // `.unwrap()` is fine here because the RBinaryExpression will always
+    //     // have a parent.
+    //     let in_for_body = ast.syntax().parent().unwrap().kind() == RSyntaxKind::R_FOR_STATEMENT
+    //         && ast.syntax().index() == 6;
 
-    if !ancestor_is_if && !ancestor_is_while && !ancestor_is_arg && !ancestor_is_for {
-        return Ok(None);
-    }
+    //     if !in_for_body {
+    //         for ancestor in ast.syntax().ancestors() {
+    //             if RBracedExpressions::can_cast(ancestor.kind()) {
+    //                 // Found braced expressions first, so skip
+    //                 result = false;
+    //                 break;
+    //             } else if RForStatement::can_cast(ancestor.kind()) {
+    //                 // Found argument first, so include
+    //                 result = true;
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     result
+    // };
 
-    let msg = if ancestor_is_if {
-        "Avoid implicit assignments in `if()` statements."
-    } else if ancestor_is_while {
-        "Avoid implicit assignments in `while()` statements."
-    } else if ancestor_is_for {
-        "Avoid implicit assignments in `for()` statements."
-    } else if ancestor_is_arg {
-        "Avoid implicit assignments in function calls."
-    } else {
-        unreachable!()
-    };
+    // if !ancestor_is_if && !ancestor_is_while && !ancestor_is_arg && !ancestor_is_for {
+    //     return Ok(None);
+    // }
 
-    let range = ast.syntax().text_trimmed_range();
-    let diagnostic = Diagnostic::new(
-        ViolationData::new("implicit_assignment".to_string(), msg.to_string()),
-        range,
-        Fix::empty(),
-    );
+    // let msg = if ancestor_is_if {
+    //     "Avoid implicit assignments in `if()` statements."
+    // } else if ancestor_is_while {
+    //     "Avoid implicit assignments in `while()` statements."
+    // } else if ancestor_is_for {
+    //     "Avoid implicit assignments in `for()` statements."
+    // } else if ancestor_is_arg {
+    //     "Avoid implicit assignments in function calls."
+    // } else {
+    //     unreachable!()
+    // };
 
-    Ok(Some(diagnostic))
+    // let range = ast.syntax().text_trimmed_range();
+    // let diagnostic = Diagnostic::new(
+    //     ViolationData::new("implicit_assignment".to_string(), msg.to_string()),
+    //     range,
+    //     Fix::empty(),
+    // );
+
+    // Ok(Some(diagnostic))
 }
