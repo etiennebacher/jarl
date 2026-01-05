abcea004be13676d472229d71055d9445734c531
etiennebacher <etienne.bacher@protonmail.com>
2026-01-05 23:31:12 +0100

    fix

--- crates/jarl-core/src/analyze/if_.rs
+++ crates/jarl-core/src/analyze/if_.rs
@@ -7,7 +7,7 @@ use crate::lints::coalesce::coalesce::coalesce;
 
 pub fn if_(r_expr: &RIfStatement, checker: &mut Checker) -> anyhow::Result<()> {
     let node = r_expr.syntax();
-    if checker.is_rule_enabled(Rule::Coalesce) && checker.should_skip_rule(node, Rule::Coalesce) {
+    if checker.is_rule_enabled(Rule::Coalesce) && !checker.should_skip_rule(node, Rule::Coalesce) {
         checker.report_diagnostic(coalesce(r_expr)?);
     }
     Ok(())
