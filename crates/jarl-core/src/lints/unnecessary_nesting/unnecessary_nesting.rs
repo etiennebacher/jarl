511a9714277341f853be91e718f128da046c9e6c
etiennebacher <etienne.bacher@protonmail.com>
2026-01-18 17:53:50 +0100

    docs

--- crates/jarl-core/src/lints/unnecessary_nesting/unnecessary_nesting.rs
+++ crates/jarl-core/src/lints/unnecessary_nesting/unnecessary_nesting.rs
@@ -5,10 +5,33 @@ use biome_rowan::{AstNode, AstNodeList};
 
 /// ## What it does
 ///
+/// This rule detects nested `if` conditions that could be gathered into a single
+/// one.
+///
 /// ## Why is this bad?
+///
+/// Nesting `if` conditions when it is not necessary may hurt readability.
+///
+/// This rule has a safe fix, in the sense that it will not change the meaning
+/// of the code. However, it may produce code that is incorrectly formatted.
 ///
 /// ## Example
 ///
+/// ```r
+/// if (x > 0) {
+///   if (y > 0) {
+///     print("x and y are greather than 0")
+///   }
+/// }
+/// ```
+///
+/// Use instead:
+///
+/// ```r
+/// if (x > 0 && y > 0) {
+///   print("x and y are greather than 0")
+/// }
+/// ```
 pub fn unnecessary_nesting(ast: &RIfStatement) -> anyhow::Result<Option<Diagnostic>> {
     let body = ast.consequence()?;
     let has_else = ast.else_clause().is_some();
