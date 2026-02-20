8be8189920df1f04e6055c00082b247f90951717
etiennebacher <etienne.bacher@protonmail.com>
2026-02-20 20:10:00 +0100

    docs: Mention that `browser` rule is deprecated

--- crates/jarl-core/src/lints/base/browser/browser.rs
+++ crates/jarl-core/src/lints/base/browser/browser.rs
@@ -10,6 +10,10 @@ pub struct Browser;
 /// Checks for lingering presence of `browser()` which should not be present in
 /// released code.
 ///
+/// **This rule is deprecated and will be removed in a future version. Use the
+/// rule [`undesirable_function`](https://jarl.etiennebacher.com/rules/undesirable_function)
+/// and configure it to report calls to `browser()` instead.**
+///
 /// ## Why is this bad?
 ///
 /// `browser()` interrupts the execution of an expression and allows the inspection
