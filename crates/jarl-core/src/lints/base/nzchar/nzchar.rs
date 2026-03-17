dd1475c1d28b71189e87b265192a4e833437722e
Maëlle Salmon <maelle.salmon@yahoo.se>
2026-03-17 16:12:29 +0100

    docs: docs

--- crates/jarl-core/src/lints/base/nzchar/nzchar.rs
+++ crates/jarl-core/src/lints/base/nzchar/nzchar.rs
@@ -8,16 +8,16 @@ pub struct NzChar;
 /// ## What it does
 ///
 /// Checks for usage of `x != ""` or `x == ""`
-//  rather than `nzchar(x)` or `!nzchar(x)`.
+///  rather than `nzchar(x)` or `!nzchar(x)`.
 ///
 /// ## Why is this bad?
 ///
 /// One crucial difference is in the default handling of `NA_character_`,
-// i.e., missing strings. `nzchar(NA_character_)` is TRUE,
-// while `NA_character_ == ""` and `nchar(NA_character_) == 0` are both NA.
-// Therefore, for strict compatibility, use `nzchar(x, keepNA = TRUE)`.
-// If the input is known to be complete (no missing entries),
-// this argument can be dropped for conciseness.
+/// i.e., missing strings. `nzchar(NA_character_)` is TRUE,
+/// while `NA_character_ == ""` and `nchar(NA_character_) == 0` are both NA.
+/// Therefore, for strict compatibility, use `nzchar(x, keepNA = TRUE)`.
+/// If the input is known to be complete (no missing entries),
+/// this argument can be dropped for conciseness.
 ///
 /// This rule comes with a unsafe fix.
 ///
---
+++ docs/rules/nzchar.md
@@ -0,0 +1,35 @@
+# nzchar
+## What it does
+
+Checks for usage of `x != ""` or `x == ""`
+ rather than `nzchar(x)` or `!nzchar(x)`.
+
+## Why is this bad?
+
+One crucial difference is in the default handling of `NA_character_`,
+i.e., missing strings. `nzchar(NA_character_)` is TRUE,
+while `NA_character_ == ""` and `nchar(NA_character_) == 0` are both NA.
+Therefore, for strict compatibility, use `nzchar(x, keepNA = TRUE)`.
+If the input is known to be complete (no missing entries),
+this argument can be dropped for conciseness.
+
+This rule comes with a unsafe fix.
+
+## Example
+
+```r
+if (x == "") {
+  message("empty string")
+}
+```
+
+Use instead:
+```r
+if (!nzchar(x)) {
+  message("empty string")
+}
+```
+
+## References
+
+See `?nzchar`
